c0131aa4f796953de67ef108dc9aa067be0addd2
ANNA STEELE <anna.a.steele@outlook.com>
2023-10-24 01:00:59 +0200

    update localise date to format with dayjs

--- utils/general.js
+++ utils/general.js
@@ -1,11 +1,12 @@
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import "dayjs/locale/de";
+
+dayjs.extend(customParseFormat); // needed for parsing date formats, see: https://mantine.dev/dates/getting-started/
+
 export const localiseDate = (date) => {
-    if (!date) return
-  let originalDate = new Date(date);
-  let options = {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-  };
-  const dateFormatter = new Intl.DateTimeFormat("de", options);
-  return dateFormatter.format(originalDate);
+  if (!date) return;
+  let originalDate = dayjs(date);
+  originalDate = originalDate.locale("de"); // set locale to German
+  return originalDate.format("DD.MM.YYYY"); // use German date format
 };
