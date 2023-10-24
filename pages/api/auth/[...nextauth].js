b572b91ed194ad9fb592a12721f40530f4c3cbd7
ANNA STEELE <anna.a.steele@outlook.com>
2023-10-24 17:47:17 +0200

    Implement with Email log in - not finished

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import GoogleProvider from "next-auth/providers/google";
+import EmailProvider from "next-auth/providers/email"
 import clientPromise from "@/db/mongodb";
 
 export const authOptions = {
@@ -24,6 +25,11 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+      maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
     }),
     // ...add more providers here
   ],
