bbe77ace50e30509b78692d5a22cb525bc988eda
ANNA STEELE <anna.a.steele@outlook.com>
2023-10-25 14:08:00 +0200

    implement log in with email link

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import GoogleProvider from "next-auth/providers/google";
-import EmailProvider from "next-auth/providers/email"
+import EmailProvider from "next-auth/providers/email";
 import clientPromise from "@/db/mongodb";
 
 export const authOptions = {
@@ -26,21 +26,34 @@ export const authOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
+    // EmailProvider({
+    //   server: process.env.EMAIL_SERVER,
+    //   from: process.env.EMAIL_FROM,
+    //   maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
+    // }),
     EmailProvider({
-      server: process.env.EMAIL_SERVER,
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
       from: process.env.EMAIL_FROM,
       maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
     }),
     // ...add more providers here
   ],
-  adapter: MongoDBAdapter(clientPromise),
+  adapter: MongoDBAdapter(clientPromise, { synchronize: false }),
 
-  // callbacks: {
-  //   async session({ session, user }) {
-  //     session.user.userId = user.id;
-  //     return session;
-  //   },
-  // },
+  callbacks: {
+    async session({ session, user }) {
+      session.user.userId = user.id;
+      return session;
+    },
+  },
 };
 
 export default NextAuth(authOptions);
