cb9c31b34ec7302be2c6611fd1ac0ef8f4a861cc
ANNA STEELE <anna.a.steele@outlook.com>
2023-10-28 14:49:15 +0200

    fix fetching, add loading/error state indicators

--- pages/[userId]/index.js
+++ pages/[userId]/index.js
@@ -1,37 +1,31 @@
 import { Button, Container, Flex } from "@mantine/core";
-import Header from "../../components/Header/Header";
-import JobsTable from "../../components/JobsTable/JobsTable";
-
-import useSWR from "swr";
-import { IconPencilPlus } from "@tabler/icons-react";
-import Link from "next/link";
-import { useEffect, useState } from "react";
-
-import { Hero } from "@/components/Hero/Hero";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import useSWR from "swr";
+import Link from "next/link";
+import Header from "../../components/Header/Header";
+import { IconPencilPlus } from "@tabler/icons-react";
+import JobsTable from "../../components/JobsTable/JobsTable";
 
 export default function UserPage() {
   const { data: session } = useSession();
-  const userId = session?.user?.userId
-  console.log("userId", userId);
-  const { data: jobs } = useSWR(`/api/${userId}/jobs`, { fallbackData: [] });
-  const [jobsAdded, setjobsAdded] = useState(false);
-  const router = useRouter();
+  const userId = session?.user?.userId;
 
-  useEffect(() => {
-    if (jobs && jobs.length > 0) {
-      return setjobsAdded(true);
-    } else {
-      return setjobsAdded(false);
-    }
-  }, [jobs]);
+  const {
+    data: jobs,
+    error,
+    isLoading,
+  } = useSWR(userId ? `/api/${userId}/jobs` : null, {
+    fallbackData: [],
+    revalidateOnFocus: false,
+  });
 
- 
+  if (!session) {
+    return null;
+  }
 
   return (
     <>
-      <Header session={session}/>
+      <Header session={session} />
       <Container
         gap="xl"
         justify="center"
@@ -40,21 +34,35 @@ export default function UserPage() {
         my="xl"
         mt="150px"
       >
-        {!jobsAdded ? (
-          <Button variant="filled" size="xl" component={Link} href={`${userId}/add`}>
-            <IconPencilPlus style={{ marginRight: "10px" }} /> Add new job
-            opportunity
-          </Button>
-        ) : (
+        {isLoading ? <p>Loading jobs list...</p> : null}
+        {error ? <p>Failed to load the jobs list</p> : null}
+        {jobs && jobs.length > 0 ? (
           <>
-            <JobsTable jobs={jobs} userId={userId}/>
+            <JobsTable jobs={jobs} userId={userId} />
             <Flex justify="flex-end">
-              <Button variant="filled" size="sm" component={Link} href={`${userId}/add`}>
+              <Button
+                variant="filled"
+                size="sm"
+                mt="50px"
+                component={Link}
+                href={`${userId}/add`}
+              >
                 + Job
               </Button>
             </Flex>
           </>
-        )}
+        ) : null}
+        {jobs && jobs.length === 0 ? (
+          <Button
+            variant="filled"
+            size="xl"
+            component={Link}
+            href={`${userId}/add`}
+          >
+            <IconPencilPlus style={{ marginRight: "10px" }} /> Add new job
+            opportunity
+          </Button>
+        ) : null}
       </Container>
     </>
   );
