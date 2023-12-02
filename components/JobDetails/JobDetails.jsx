4c685cfd2af5d3c8f57123499ef9fe8c2e8ece81
ANNA STEELE <anna.a.steele@outlook.com>
2023-12-02 22:33:36 +0100

    update styles for Job details page (mobile)

--- components/JobDetails/JobDetails.jsx
+++ components/JobDetails/JobDetails.jsx
@@ -1,8 +1,7 @@
 import {
-  Anchor,
   Button,
+  Center,
   Flex,
-  Group,
   Modal,
   Paper,
   Stack,
@@ -16,7 +15,6 @@ import {
   IconCircleX,
   IconExternalLink,
   IconFileDescription,
-  IconLink,
 } from "@tabler/icons-react";
 import stageColorSetter, { localiseDate } from "../../utils/general";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -26,6 +24,7 @@ import {
 } from "@fortawesome/free-regular-svg-icons";
 import DOMPurify from "dompurify";
 import { useDisclosure } from "@mantine/hooks";
+import classes from "./JobDetails.module.css";
 
 export default function JobDetails({ job }) {
   const {
@@ -62,126 +61,144 @@ export default function JobDetails({ job }) {
   }
 
   return (
-    <Paper shadow="xs" p="xl" withBorder maw="768px" w={"100%"}>
-      <Flex gap="20px" wrap={"nowrap"}>
-        <Stack maw={500}>
-          <Stack gap={0}>
-            <Text fz="sm">Role:</Text>
-            <Text>
-              <strong>{role}</strong>
-            </Text>
-          </Stack>
+    <Flex className={classes.container}>
+      <Paper
+        className={classes.paper}
+        shadow="xs"
+        withBorder
+        w={"100%"}
+        mx="auto"
+      >
+        <Center>
+          <Flex wrap={"nowrap"} gap={16}>
+            <Stack maw={500}>
+              <Stack gap={0}>
+                <Text fz="sm">Role:</Text>
+                <Text>
+                  <strong>{role}</strong>
+                </Text>
+              </Stack>
 
-          <Stack gap={0}>
-            <Text fz="sm">Company:</Text>
-            <Text>
-              <strong>{company}</strong>
-            </Text>
-          </Stack>
+              <Stack gap={0}>
+                <Text fz="sm">Company:</Text>
+                <Text>
+                  <strong>{company}</strong>
+                </Text>
+              </Stack>
 
-          <Stack gap={0}>
-            <Text fz="sm">Location:</Text>
-            <Text>
-              <strong>{location}</strong>
-            </Text>
-          </Stack>
+              <Stack gap={0}>
+                <Text fz="sm">Location:</Text>
+                <Text>
+                  <strong>{location}</strong>
+                </Text>
+              </Stack>
 
-          <Group>
-            <Button
-              leftSection={<IconExternalLink />}
-              variant="outline"
-              component="a"
-              href={`${URL}`}
-              target="_blank"
-            >
-              Go to the job post
-            </Button>
+              <Flex
+                className={classes.buttonsContainer}
+                maw="fit-content"
+                gap={16}
+              >
+                <Button
+                  leftSection={<IconExternalLink />}
+                  variant="outline"
+                  component="a"
+                  href={`${URL}`}
+                  target="_blank"
+                >
+                  Go to the job post
+                </Button>
 
-            <Button
-              leftSection={<IconFileDescription />}
-              variant="outline"
-              onClick={open}
-              px={20}
-            >
-              Role description
-            </Button>
-          </Group>
+                <Button
+                  leftSection={<IconFileDescription />}
+                  variant="outline"
+                  onClick={open}
+                  px={20}
+                >
+                  Role description
+                </Button>
+              </Flex>
 
-          <Modal
-            opened={opened}
-            onClose={close}
-            title={`${role} at ${company}`}
-            size="70%"
-            styles={{
-              title: {
-                fontSize: "2rem",
-                color: "var(--mantine-color-blue-filled)",
-              },
-            }}
-          >
-            <div dangerouslySetInnerHTML={{ __html: sanitizedDescription }} />
-          </Modal>
+              <Modal
+                opened={opened}
+                onClose={close}
+                title={`${role} at ${company}`}
+                size="90%"
+                styles={{
+                  title: {
+                    fontSize: "2rem",
+                    color: "var(--mantine-color-blue-filled)",
+                  },
+                }}
+              >
+                <div
+                  dangerouslySetInnerHTML={{ __html: sanitizedDescription }}
+                />
+              </Modal>
 
-          <Stack gap={0}>
-            <Text fz="sm">Contact person:</Text>
-            <Text>
-              <strong>{contactPerson}</strong>
-            </Text>
-          </Stack>
+              <Stack gap={0}>
+                <Text fz="sm">Contact person:</Text>
+                <Text>
+                  <strong>{contactPerson}</strong>
+                </Text>
+              </Stack>
 
-          <Stack gap={0}>
-            <Text fz="sm">Contact details:</Text>
-            <Text>
-              <strong>{contactDetails}</strong>
-            </Text>
-          </Stack>
+              <Stack gap={0}>
+                <Text fz="sm">Contact details:</Text>
+                <Text>
+                  <strong>{contactDetails}</strong>
+                </Text>
+              </Stack>
 
-          <Stack gap={0} style={{ whiteSpace: "pre-wrap" }}>
-            <Text fz="sm">Notes:</Text>
-            <Text>
-              <em>{notes}</em>
-            </Text>
-          </Stack>
-        </Stack>
+              <Stack gap={0} style={{ whiteSpace: "pre-wrap" }}>
+                <Text fz="sm">Notes:</Text>
+                <Text>
+                  <em>{notes}</em>
+                </Text>
+              </Stack>
+            </Stack>
 
-        <Stack ml="auto">
-          <Timeline active={10} bulletSize={30} lineWidth={2} color="blue">
-            <Timeline.Item
-              key={_id}
-              mb="20px"
-              bullet={<IconCircleCheck size={30} />}
-              lineVariant="solid"
-              title="Applied on:"
-            >
-              <Text size="xs" mt={4}>
-                {localiseDate(appliedOn)}
-              </Text>
-            </Timeline.Item>
-
-            {stages?.map((stage, index) => {
-              const { _id, stageName, stageDate } = stage;
-
-              return (
+            <Stack ml="auto">
+              <Timeline active={10} bulletSize={30} lineWidth={2} color="blue">
                 <Timeline.Item
                   key={_id}
+                  className={classes.timelineItem1}
                   mb="20px"
-                  bullet={setIcon(stageName)}
+                  bullet={<IconCircleCheck size={30} />}
                   lineVariant="solid"
-                  title={`Stage ${index + 1}`}
-                  color={stageColorSetter(stageName, index)}
+                  title="Applied on:"
                 >
-                  <Text size="sm">{stageName}</Text>
-
                   <Text size="xs" mt={4}>
-                    {localiseDate(stageDate)}
+                    {localiseDate(appliedOn)}
                   </Text>
                 </Timeline.Item>
-              );
-            })}
-          </Timeline>
-        </Stack>
-      </Flex>
-    </Paper>
+
+                {stages?.map((stage, index) => {
+                  const { _id, stageName, stageDate } = stage;
+
+                  return (
+                    <Timeline.Item
+                      key={_id}
+                      className={classes.timelineItemRest}
+                      mb="20px"
+                      bullet={setIcon(stageName)}
+                      lineVariant="solid"
+                      title={`Stage ${index + 1}`}
+                      color={stageColorSetter(stageName, index)}
+                    >
+                      <Text size="sm">{stageName}</Text>
+
+                      <Text size="xs" mt={4}>
+                        {localiseDate(stageDate)}
+                      </Text>
+                    </Timeline.Item>
+                  );
+                })}
+              </Timeline>
+            </Stack>
+          </Flex>
+        </Center>
+      </Paper>
+    </Flex>
   );
 }
 
--- pages/[userId]/jobs/[id]/index.js
+++ pages/[userId]/jobs/[id]/index.js
@@ -1,4 +1,4 @@
-import { Button, Group, Modal, Stack, Title } from "@mantine/core";
+import { Button, Flex, Group, Modal, Title } from "@mantine/core";
 import Header from "../../../../components/Header/Header";
 import JobDetails from "../../../../components/JobDetails/JobDetails";
 import { useRouter } from "next/router.js";
@@ -44,61 +44,63 @@ export default function JobDetailsPage() {
     <>
       <Header session={session} />
       <GoBackButton />
-      <Stack align="center" mx={20}>
+
+      <Flex justify="center">
         <Title>Job Details</Title>
-        {error ? <p>Failed to load job details...</p> : null}
-        {!job && isLoading ? <JobDetailsSkeleton /> : null}
-        {job ? (
-          <>
-            <JobDetails job={job} />
-            <Group justify="center" mt="md" mb="100px">
-              <Button
-                variant="outline"
-                size="sm"
-                component="a"
-                href={`/${userId}/jobs/${id}/edit`}
-                px={18}
-                leftSection={
-                  <IconPencil color="var(--mantine-color-blue-outline)" />
-                }
-              >
-                Edit
+      </Flex>
+
+      {error ? <p>Failed to load job details...</p> : null}
+      {!job && isLoading ? <JobDetailsSkeleton /> : null}
+      {job ? (
+        <>
+          <JobDetails job={job} />
+          <Group justify="center" mt="md" mb="100px">
+            <Button
+              variant="outline"
+              size="sm"
+              component="a"
+              href={`/${userId}/jobs/${id}/edit`}
+              px={18}
+              leftSection={
+                <IconPencil color="var(--mantine-color-blue-outline)" />
+              }
+            >
+              Edit
+            </Button>
+            <Button
+              variant="outline"
+              color="rgba(255, 87, 87, 1)"
+              size="sm"
+              px="9px"
+              onClick={open}
+              leftSection={<IconTrash color="rgba(255, 87, 87, 1)" />}
+            >
+              Delete
+            </Button>
+          </Group>
+          <Modal
+            opened={opened}
+            onClose={close}
+            withCloseButton={false}
+            align="center"
+            centered
+          >
+            Are you sure you want to delete this job?
+            <Group justify="center" gap={16} mt="md">
+              <Button onClick={close} leftSection={<IconX />} px={17}>
+                No
               </Button>
               <Button
-                variant="outline"
+                onClick={deleteJob}
                 color="rgba(255, 87, 87, 1)"
-                size="sm"
-                px="9px"
-                onClick={open}
-                leftSection={<IconTrash color="rgba(255, 87, 87, 1)" />}
+                leftSection={<IconCheck />}
               >
-                Delete
+                Yes
               </Button>
             </Group>
-            <Modal
-              opened={opened}
-              onClose={close}
-              withCloseButton={false}
-              align="center"
-              centered
-            >
-              Are you sure you want to delete this job?
-              <Group justify="center" gap={16} mt="md">
-                <Button onClick={close} leftSection={<IconX />} px={17}>
-                  No
-                </Button>
-                <Button
-                  onClick={deleteJob}
-                  color="rgba(255, 87, 87, 1)"
-                  leftSection={<IconCheck />}
-                >
-                  Yes
-                </Button>
-              </Group>
-            </Modal>
-          </>
-        ) : null}
-      </Stack>
+          </Modal>
+        </>
+      ) : null}
     </>
   );
 }
