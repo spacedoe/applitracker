9abddf90ceb61b0da5abe95baf39786650a47a1a
ANNA STEELE <anna.a.steele@outlook.com>
2023-12-03 21:20:32 +0100

    Add styling to JobDetailsSkeleton (mobile)

--- components/JobDetails/JobDetailsSkeleton.jsx
+++ components/JobDetails/JobDetailsSkeleton.jsx
@@ -1,56 +1,67 @@
 import { Flex, Paper, Stack, Text, Skeleton } from "@mantine/core";
+import classes from "./JobDetailsSkeleton.module.css";
 
 export default function JobDetailsSkeleton() {
   return (
-    <Paper shadow="xs" p="xl" withBorder maw={768} w="100%">
-      <Flex gap="20px" mx="lg">
-        <Stack w="100%">
-          <Flex>
-            <Text>Role:</Text>
-            <Skeleton height={18} mt={6} ml="sm" radius="xl" />
-          </Flex>
+    <Flex className={classes.container}>
+      <Paper
+        className={classes.paper}
+        shadow="xs"
+        withBorder
+        w="100%"
+        mx="auto"
+      >
+        <Flex gap="20px" mx="xs">
+          <Stack w="100%">
+            <Flex>
+              <Text>Role:</Text>
+              <Skeleton height={18} mt={6} ml="sm" radius="xl" />
+            </Flex>
 
-          <Flex>
-            <Text>Company:</Text>
-            <Skeleton height={18} mt={6} ml="sm" radius="xl" />
-          </Flex>
+            <Flex>
+              <Text>Company:</Text>
+              <Skeleton height={18} mt={6} ml="sm" radius="xl" />
+            </Flex>
 
-          <Flex>
-            <Text>Location:</Text>
-            <Skeleton height={18} mt={6} ml="sm" radius="xl" />
-          </Flex>
+            <Flex>
+              <Text>Location:</Text>
+              <Skeleton height={18} mt={6} ml="sm" radius="xl" />
+            </Flex>
 
-          <Flex>
-            <Text>URL:</Text>
-            <Skeleton height={18} mt={6} ml="sm" radius="xl" />
-          </Flex>
+            <Flex>
+              <Text>URL:</Text>
+              <Skeleton height={18} mt={6} ml="sm" radius="xl" />
+            </Flex>
 
-          <Flex>
-            <Text>Description:</Text>
-            <Skeleton height={18} mt={6} ml="sm" radius="xl" />
-          </Flex>
+            <Flex>
+              <Text>Description:</Text>
+              <Skeleton height={18} mt={6} ml="sm" radius="xl" />
+            </Flex>
             <Stack>
-
-            <Skeleton height={18} mt={6}  radius="xl" />
-            <Skeleton height={18} mt={6}  radius="xl" />
-            <Skeleton height={18} mt={6}  radius="xl" />
-            <Skeleton height={18} mt={6}  radius="xl" />
+              <Skeleton height={18} mt={6} radius="xl" />
+              <Skeleton height={18} mt={6} radius="xl" />
+              <Skeleton height={18} mt={6} radius="xl" />
+              <Skeleton height={18} mt={6} radius="xl" />
             </Stack>
 
-          <Flex>
-            <Text w="165px">Contact person:</Text>
-            <Skeleton height={18} mt={6} ml="sm" radius="xl" />
-          </Flex>
-          <Flex>
-            <Text w="165px">Contact details:</Text>
-            <Skeleton height={18} mt={6} ml="sm" radius="xl" />
-          </Flex>
-          <Flex>
-            <Text>Notes:</Text>
-            <Skeleton height={18} mt={6} ml="sm" radius="xl" />
-          </Flex>
-        </Stack>
-      </Flex>
-    </Paper>
+            <Flex>
+              <Text style={{ whiteSpace: "nowrap" }}> Contact person:</Text>
+              <Skeleton height={18} mt={6} ml="sm" radius="xl" />
+            </Flex>
+            <Flex>
+              <Text style={{ whiteSpace: "nowrap" }}>Contact details:</Text>
+              <Skeleton height={18} mt={6} ml="sm" radius="xl" />
+            </Flex>
+            <Flex>
+              <Text>Notes:</Text>
+              <Skeleton height={18} mt={6} ml="sm" radius="xl" />
+            </Flex>
+            <Stack>
+              <Skeleton height={18} mt={6} radius="xl" />
+            </Stack>
+          </Stack>
+        </Flex>
+      </Paper>
+    </Flex>
   );
 }
