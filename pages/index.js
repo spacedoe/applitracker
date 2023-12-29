47b1d2d34db1f672e87603f0c1037ef3365f5f25
ANNA STEELE <anna.a.steele@outlook.com>
2023-12-29 21:08:23 +0100

    fix footer

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
 import Footer from "@/components/Footer/Footer";
 import { Hero } from "@/components/Hero/Hero";
+import { Flex } from "@mantine/core";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import classes from "./index.module.css";
 
 export default function HomePage() {
   const { data: session, status } = useSession();
   const userId = session?.user?.userId;
   const router = useRouter();
 
-  if (status === "unauthenticated") return (<>
-  <Hero />
-  <Footer/>
-  </>);
+  if (status === "unauthenticated")
+    return (
+      <Flex className={classes.container}>
+        <Hero />
+        <Footer />
+      </Flex>
+    );
 
   if (session && status === "authenticated") router.push(`/${userId}`);
 
